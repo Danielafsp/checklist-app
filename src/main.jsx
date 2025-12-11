19a432c723d50c2566411e1069915be43ac3c2a9
Danielafsp <135218019+Danielafsp@users.noreply.github.com>
2025-12-11 14:11:11 +0000

    Add React Route and main login selection

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import ClientLogin from "./pages/ClientLogin.jsx";
+import AdminLogin from "./pages/AdminLogin.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/client-login" element={<ClientLogin />} />
+      <Route path="/admin-login" element={<AdminLogin />} />
+    </Routes>
+  </BrowserRouter>
+);
