01cbb8cc4befbb1955dc70f185ee139046b2bea9
Danielafsp <135218019+Danielafsp@users.noreply.github.com>
2025-12-11 18:23:10 +0000

    Add login page + client & admin areas

---
+++ src/pages/AdminDashboard.jsx
@@ -0,0 +1,3 @@
+export default function AdminDashboard() {
+  return <h1>Admin Dashboard - Here Admin will see results and reports</h1>;
+}
---
+++ src/pages/ClientDashboard.jsx
@@ -0,0 +1,3 @@
+export default function ClientDashboard() {
+  return <h1>Client Dashboard - Here the client will fill the checklist</h1>;
+}
