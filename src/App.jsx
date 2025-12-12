888afce13db0686709d454fb0b9cd5ff8b424afc
Danielafsp <135218019+Danielafsp@users.noreply.github.com>
2025-12-12 16:06:16 +0000

    Clean App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home.jsx";
-import ClientLogin from "./pages/ClientLogin.jsx";
-import AdminLogin from "./pages/AdminLogin.jsx";
 import ClientDashboard from "./pages/ClientDashboard.jsx";
 import AdminDashboard from "./pages/AdminDashboard.jsx";
+
 import PromptChecklist from "./pages/Prompt/PromptChecklist.jsx";
-import Area from "./pages/Prompt/PromptArea.jsx";
+import PromptArea from "./pages/Prompt/PromptArea.jsx";
+
 import SubdewChecklist from "./pages/Subdew/SubdewChecklist.jsx";
+import SubdewArea from "./pages/Subdew/SubdewArea.jsx";
+
 import FrugalUploader from "./pages/Frugal/FrugalUploader.jsx";
 
 export default function App() {
@@ -16,9 +18,13 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/client" element={<ClientDashboard />} />
       <Route path="/admin" element={<AdminDashboard />} />
+
       <Route path="/prompt" element={<PromptChecklist />} />
-      <Route path="/prompt/area/:areaId" element={<Area />} />
+      <Route path="/prompt/:areaId" element={<PromptArea />} />
+
       <Route path="/subdew" element={<SubdewChecklist />} />
+      <Route path="/subdew/:areaId" element={<SubdewArea />} />
+
       <Route path="/frugal" element={<FrugalUploader />} />
     </Routes>
   );
