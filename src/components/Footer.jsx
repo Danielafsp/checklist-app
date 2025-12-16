fcab016a4c6e1e4ce27cc36275feb7c46681df89
Danielafsp <135218019+Danielafsp@users.noreply.github.com>
2025-12-16 17:57:45 +0000

    Add style to Home + Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import "../styles/Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <a href="phone:4032507999" className="footer-link">
+        (403) 250-7999
+      </a>
+
+      <span className="footer-separator">•</span>
+
+      <a href="mailto:info@fortsands.com" className="footer-link">
+        info@fortsands.com
+      </a>
+    </footer>
+  );
+}
