f836d9671b7f1e330c2f2acc84c7d9c96a0dda3b
Danielafsp <135218019+Danielafsp@users.noreply.github.com>
2025-12-16 16:40:25 +0000

    Add prompt questions

---
+++ src/data/promptQuestions.js
@@ -0,0 +1,17 @@
+export const promptQuestions = {
+  1: [
+    {
+      id: 1,
+      text: "Gutters in good condition; no cracks or holes, or clogging",
+    },
+    { id: 2, text: "Gutters properly sloped; water is not pooling in gutter" },
+    { id: 3, text: "Downspouts securely attached; directing water away" },
+    { id: 4, text: "Drainage systems, such as catch basins, are not clogged" },
+  ],
+  2: [
+    { id: 1, text: "Roof with missing/damaged shingles, cracks/splits?" },
+    { id: 2, text: "Roof free of water damage? Any areas sagging?" },
+    { id: 3, text: "Flashing around chimneys and vents is waterproof " },
+    { id: 4, text: "Quality of roof edges -- look for lifting or rot" },
+  ],
+};
--- src/pages/Prompt/PromptArea.jsx
+++ src/pages/Prompt/PromptArea.jsx
@@ -1,12 +1,49 @@
 import { useParams } from "react-router-dom";
+import { promptQuestions } from "../../data/promptQuestions";
 
 export default function PromptArea() {
   const { areaId } = useParams();
 
+  const questions = promptQuestions[areaId];
+
   return (
-    <div>
+    <div style={{ padding: "1rem" }}>
       <h1>Area {areaId}</h1>
-      <p>This page will show the checklist questions.</p>
+
+      {!questions && <p>No questions found for this area.</p>}
+
+      <ul>
+        {questions?.map((q) => (
+          <li key={q.id} style={{ marginBottom: "1rem" }}>
+            <p>
+              <strong>{q.text}</strong>
+            </p>
+
+            <label>rating: </label>
+            <select>
+              <option value="">Select</option>
+              <option value="1">1 - Poor</option>
+              <option value="2">2 - Fair</option>
+              <option value="3">3 - Good</option>
+              <option value="4">4 - Very Good</option>
+              <option value="5">5 - Excellent</option>
+            </select>
+
+            <br />
+            <br />
+
+            <label>Notes:</label>
+            <br />
+            <textarea rows="3" style={{ width: "100%" }} />
+
+            <br />
+            <br />
+
+            <label>Photo:</label>
+            <input type="file" accpet="image/*" />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
