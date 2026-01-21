14dd1a86ef4155b44f339f969e8dd3cbfbfbbd8c
Danielafsp <135218019+Danielafsp@users.noreply.github.com>
2026-01-21 16:55:36 +0000

    Add photos

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -1,3 +1,18 @@
+import amrita from "../assets/team/amrita.png";
+import bret from "../assets/team/Bret.png";
+import corey from "../assets/team/Corey.png";
+import david from "../assets/team/David.png";
+import davin from "../assets/team/Davin.png";
+import easton from "../assets/team/easton.png";
+import kelly from "../assets/team/Kelly.png";
+import lukasz from "../assets/team/Lukasz.png";
+import max from "../assets/team/Max.png";
+import randy from "../assets/team/Randy.png";
+import rick from "../assets/team/Rick.png";
+import simon from "../assets/team/Simon.png";
+import trent from "../assets/team/Trent.png";
+import zach from "../assets/team/Zach.png";
+
 export const teamData = [
   {
     id: 1,
@@ -6,6 +21,7 @@ export const teamData = [
     email: "zjacobs@fortsands.com",
     phone: "403-333-6051",
     city: "Calgary",
+    image: zach,
   },
   {
     id: 2,
@@ -14,6 +30,7 @@ export const teamData = [
     email: "bmckay@fortsands.com",
     phone: "403-669-5270",
     city: "Calgary",
+    image: bret,
   },
   {
     id: 3,
@@ -22,6 +39,7 @@ export const teamData = [
     email: "tleinenweber@fortsands.com",
     phone: "403-827-7373",
     city: "Calgary",
+    image: trent,
   },
   {
     id: 4,
@@ -30,6 +48,7 @@ export const teamData = [
     email: "swallis@fortsands.com",
     phone: "368-997-5929",
     city: "Calgary",
+    image: simon,
   },
   {
     id: 6,
@@ -38,6 +57,7 @@ export const teamData = [
     email: "dlayton@fortsands.com",
     phone: "403-813-2746",
     city: "Calgary",
+    image: davin,
   },
   {
     id: 7,
@@ -46,6 +66,7 @@ export const teamData = [
     email: "rcaravaggio@fortsands.com",
     phone: "403-816-7084",
     city: "Calgary",
+    image: randy,
   },
   {
     id: 8,
@@ -54,6 +75,7 @@ export const teamData = [
     email: "crosta@fortsands.com",
     phone: "587-991-9597",
     city: "Calgary",
+    image: corey,
   },
   {
     id: 9,
@@ -62,6 +84,7 @@ export const teamData = [
     email: "damour@fortsands.com",
     phone: "780-699-5532",
     city: "Edmonton",
+    image: david,
   },
   {
     id: 10,
@@ -70,14 +93,16 @@ export const teamData = [
     email: "kgraham@fortsands.com",
     phone: "587-335-5899",
     city: "Edmonton",
+    image: kelly,
   },
   {
     id: 11,
-    name: "Kraig Bast",
-    title: "Director, Corporate Growth",
-    email: "kbast@fortsands.com",
-    phone: "306-450-5115",
-    city: "Regina",
+    name: "Easton Carriere",
+    title: "I.T Manager & Site Safety Officer",
+    email: "ecarrier@fortsands.com",
+    phone: "-",
+    city: "Calgary",
+    image: easton,
   },
   {
     id: 12,
@@ -86,6 +111,7 @@ export const teamData = [
     email: "rochitwa@fortsands.com",
     phone: "306-580-6191",
     city: "Saskatoon",
+    image: rick,
   },
   {
     id: 13,
@@ -94,6 +120,7 @@ export const teamData = [
     email: "lzielinski@fortsands.com",
     phone: "587-225-9730",
     city: "Calgary",
+    image: lukasz,
   },
   {
     id: 14,
@@ -102,6 +129,7 @@ export const teamData = [
     email: "asekhon@fortsands.com",
     phone: "587-837-5777",
     city: "Calgary",
+    image: amrita,
   },
   {
     id: 15,
@@ -110,5 +138,6 @@ export const teamData = [
     email: "mkovalenko@fortsands.com",
     phone: "587-969-2622",
     city: "Calgary",
+    image: max,
   },
 ];
